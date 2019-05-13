99beb72d2232d7dbcb95a45f2b2f7935b3245619
Erick Oliveira <erik.inmetrics@stelo.com.br>
2019-05-13 18:31:55 -0300

    Ajustes no arquivo models

--- aula_ORM/aula_ORM/core/models.py
+++ aula_ORM/aula_ORM/core/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Curso(models.Model):
-	nome = models.CharField("Nome", max_length=50)
-	sigla = models.CharField("Sigla", max_length=5)
-	etiqueta = models.SlugField("Etiqueta", max_length=50)
+class Register(models.Model):
+	nome = models.CharField("Nome", max_length=30)
+	email = models.EmailField("Email", max_length=30)
+	data_nascimento = models.DataField("Data_nascimento")
+	celular = models.IntegerField("Celular", max_length=50)
+	usuario = models.SlugField("Usuario", max_length=15)
+	senha = models.SlugField("Senha", max_length=15)
+	endereco = models.SlugField("Endereco", max_length=30)
+	cidade = models.SlugField("Cidade", max_length=30)
+	estado = models.SlugField("Estado", max_length=2)
+	cep = models.SlugField("Cep", max_length=10)
 	
 	def __str__(self):
 		return self.nome
